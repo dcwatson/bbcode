06105ce788fa9558dc92dedbb66b5fb5c09ba9d0
Dan Watson <dcwatson@gmail.com>
2011-05-29 10:57:10 -0400

    Add version info and a setup script

--- bbcode.py
+++ bbcode.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+__version_info__ = (1, 0, 0)
+__version__ = '.'.join(str(i) for i in __version_info__)
+
 import re
 
 # Taken from http://daringfireball.net/2010/07/improved_regex_for_matching_urls
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bbcode',
+      version='1.0.0',
+      description='A pure python bbcode parser and formatter.',
+      author='Dan Watson',
+      author_email='dcwatson@gmail.com',
+      url='https://bitbucket.org/dcwatson/bbcode',
+      py_modules=['bbcode'],
+     )
