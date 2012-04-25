9fd1736254fa8e2d7d377246e3a48dd0ec40e666
Alexander Fedorov@winwork <Alexander Fedorov@winwork>
2012-04-25 14:02:27 +0400

    2to3

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
+import sys
+
+
+extra = {}
+if sys.version_info >= (3, 0):
+    extra["use_2to3"] = True
 
 setup(
     name='bbcode',
@@ -14,6 +20,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        "Programming Language :: Python :: 3",
         'Topic :: Text Processing :: Markup',
-    ]
+        ],
+    **extra
 )
